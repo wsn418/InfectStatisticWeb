72d802f06d7f8b5cddaec3ddb8cbd681a6b17ec8
test_user <test_user@qq.com>
2020-03-14 01:38:11 +0800

    BUG修改

--- 2019-nCov/src/data/config_map.js
+++ 2019-nCov/src/data/config_map.js
@@ -11,7 +11,7 @@ export default function buildMapConfig (province, data) {
       left: province ? '3%' : 'auto',
       inRange: {
         color: [
-          '#ffc0b2',
+          '#ffc0b1',
           '#ff8c71',
           '#ef1717',
           '#9c0505'
